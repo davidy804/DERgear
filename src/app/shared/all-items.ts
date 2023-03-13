c3567825bafd3e5eb8bb4a4e1c7c0ab0bcdb53b8
David Yuan <yua002@henrico.us>
2023-03-13 13:38:25 -0400

    update

--- src/app/shared/all-items.ts
+++ src/app/shared/all-items.ts
@@ -4,100 +4,478 @@ import { MainModel } from "./main-model";
 // 1-99 = mice
 // 100-199 = mousepads
 // 200-299 = keyboards
-// 300-399 = headphones
+// 300-399 = audio
 // 400-499 = monitors
 // 500-599 = edc
 // 600-699 = other
 
 export const AllItems: MainModel[] = [
+//////// Mice ////////
 {
-  id: '1',
-  name: 'first item',
-  description: 'description',
-  link: '#',
+  id: '94',
+  type: 'Mouse',
+  name: 'Orochi v2',
+  description: 'Claw / Small',
   imgSrc: './assets/images/test.png',
   details: [
     {
       imgSrc: '',
-      caption: 'review text'
+      caption: `
+      <strong>Specs:</strong> <br> Sensor: 3365 <br> Switches: Razer Optical <br> Weight: 55g <br> Wireless: 2.4hz or Bluetooth <br> Battery: AA or AAA
+      <br> <br>
+      <strong>Shape:</strong> <br> This small ergo egg shape is weird. It is small so fingertip makes sense, but the side grooves make it hard to fingertip. The ergo shape makes you want to palm or claw the mouse. It is small, but wide with a small/thin back hump. Finding a comfortable grip on this mouse is difficult. Adding grip tape to it helps fill out the ergo shape. I prefer to claw this mouse over fingertip because of its wide body. Despite its small size, this mouse is for medium hands.
+      <br> <br>
+      <strong>Buttons:</strong> <br> The M1 and M2 buttons feel good and are very tactile. They are on the heavier side. I prefer light clicks, but these are not bad. The side buttons are very good. The scroll wheel is loud, heavy, and has well defined bumps. The scroll wheel broke on me. It stopped scrolling. I sent it to Razer and they replaced the mouse for free.
+      <br> <br>
+      <strong>Value:</strong> <br> This mouse is not worth it for the retail price of $80. I would buy it again at the sale price (~$40).
+      <br> <br>
+      <strong>Conclusions:</strong> <br> This is a budget travel mouse that can be used for gaming. The bluetooth mode, AA/AAA battery, small size makes it perfect for traveling or using it with a laptop. For gaming, it is alright. It has Razer's technology (responsive sensor and optical switches). It feels like a budget mouse. The buttons are loud. The shape and coating is not comfortable and feels slippery. The removable top shell makes the mouse creak. This is my current main for FPS games because it is wireless.
+      <br> <br>
+      <strong>Score:</strong> <br> 6/10
+      `
     }
   ],
 },
 {
-  id: '100',
-  name: 'second item',
-  description: 'description',
-  link: '#',
-  imgSrc: './assets/images/main2.png',
-  details: [
-    {
-      imgSrc: '',
-      caption: 'review text'
-    }
-  ],
-},
-{
-  id: '200',
-  name: 'three item',
-  description: 'description',
-  link: '#',
+  id: '95',
+  type: 'Mouse',
+  name: 'Razer Viper Mini',
+  description: 'Claw / Small',
   imgSrc: './assets/images/test.png',
   details: [
     {
       imgSrc: '',
-      caption: 'review text'
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Shape:</strong> <br> This is the god shape. For small hands, every grip feels comfortable. I only have one problem with the shape. It is a little too long. When I claw the mouse, the back hump is not present enough in my palm unless I angle the mouse. I've gotten used to an angle sensor, but I can see someone else having issues with this. The mouse is also very narrow. I like this, but someone with larger hands may not. For fingertip, this mouse shape is perfect.
+      <br> <br>
+      <strong>Buttons:</strong> <br> The M1 and M2 buttons feel good, but will side to side creak after a long time. The side buttons are a little mushy. The scroll wheel is light with lightly defined bumps. My scroll wheel is showing its age. The scrolling and button is no longer consistent. It randomly scrolls, scrolls in opposite directions sometimes, and button press doesn't always work.
+      <br> <br>
+      <strong>Value:</strong> <br> This is the best bang for your buck. This mouse goes on sale for ~$20. It is a lot of people's end game. I have been using it for years with heavy use and it only has minor issues.
+      <br> <br>
+      <strong>Conclusions:</strong> <br> This is the most recommended and popular mouse in the mouse community. It is very cheap, has a great shape, has good qc, and sold everywhere. The problem with this mouse is that it is wired and has a high lift off distance. This is my main mouse for everything except FPS games, because of its wire.
+      <br> <br>
+      <strong>Score:</strong> <br> 9/10
+      `
     }
   ],
 },
 {
-  id: '300',
-  name: 'four item',
-  description: 'description',
-  link: '#',
-  imgSrc: './assets/images/main4.png',
-  details: [
-    {
-      imgSrc: '',
-      caption: 'review text'
-    }
-  ],
-},
-{
-  id: '400',
-  name: 'five item',
-  description: 'description',
-  link: '#',
+  id: '96',
+  type: 'Mouse',
+  name: 'Finalmouse Air58',
+  description: 'Palm / Large',
   imgSrc: './assets/images/test.png',
   details: [
     {
       imgSrc: '',
-      caption: 'review text'
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Shape:</strong> <br>
+      <br> <br>
+      <strong>Buttons:</strong> <br>
+      <br> <br>
+      <strong>Value:</strong> <br>
+      <br> <br>
+      <strong>Conclusions:</strong> <br>
+      <br> <br>
+      <strong>Score:</strong> <br>
+      `
     }
   ],
 },
 {
-  id: '500',
-  name: 'six item',
-  description: 'description',
-  link: '#',
+  id: '97',
+  type: 'Mouse',
+  name: 'Logitech G203',
+  description: 'Claw / Small',
   imgSrc: './assets/images/test.png',
   details: [
     {
       imgSrc: '',
-      caption: 'review text'
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Shape:</strong> <br>
+      <br> <br>
+      <strong>Buttons:</strong> <br>
+      <br> <br>
+      <strong>Value:</strong> <br>
+      <br> <br>
+      <strong>Conclusions:</strong> <br>
+      <br> <br>
+      <strong>Score:</strong> <br>
+      `
     }
   ],
 },
+{
+  id: '98',
+  type: 'Mouse',
+  name: 'Finalmouse Sunset',
+  description: 'Palm / Large',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Shape:</strong> <br>
+      <br> <br>
+      <strong>Buttons:</strong> <br>
+      <br> <br>
+      <strong>Value:</strong> <br>
+      <br> <br>
+      <strong>Conclusions:</strong> <br>
+      <br> <br>
+      <strong>Score:</strong> <br>
+      `
+    }
+  ],
+},
+{
+  id: '99',
+  type: 'Mouse',
+  name: 'Logitech G502',
+  description: 'Palm / Large',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Shape:</strong> <br>
+      <br> <br>
+      <strong>Buttons:</strong> <br>
+      <br> <br>
+      <strong>Value:</strong> <br>
+      <br> <br>
+      <strong>Conclusions:</strong> <br>
+      <br> <br>
+      <strong>Score:</strong> <br>
+      `
+    }
+  ],
+},
+//////// Mousepads ////////
+{
+  id: '195',
+  type: 'Mousepad',
+  name: 'LGG Saturn',
+  description: 'Control / Hybrid',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '196',
+  type: 'Mousepad',
+  name: 'Aqua Control Plus',
+  description: 'Control / Hybrid',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '197',
+  type: 'Mousepad',
+  name: 'PFERDEKI',
+  description: 'Speed / Hybrid',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '198',
+  type: 'Mousepad',
+  name: 'Cooler Master MP510',
+  description: 'Hybrid / Codura',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '199',
+  type: 'Mousepad',
+  name: 'SteelSeries QCK+',
+  description: 'Control / Cloth',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+//////// Keyboards ////////
+{
+  id: '296',
+  type: 'Keyboard',
+  name: 'IV Works AV4',
+  description: 'KTT Roses / NicePBT Beach Day',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '297',
+  type: 'Keyboard',
+  name: 'WindX65',
+  description: 'Gateron Box Pinks / NicePBT Classic Beige',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '298',
+  type: 'Keyboard',
+  name: 'WS Ikki Aurora R2',
+  description: 'WS Silent Linears / DMK Ghost',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '299',
+  type: 'Keyboard',
+  name: 'GMMK PRO',
+  description: 'Cherry MX Browns / PBTFans WoB',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+//////// Audio ////////
+{
+  id: '309',
+  type: 'Audio',
+  name: 'Audio Technica ATH-AD900x',
+  description: 'Balanced / Open',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '318',
+  type: 'Audio',
+  name: 'Apple Airpods Pro 2',
+  description: 'V-Shape / Wireless IEM',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '319',
+  type: 'Audio',
+  name: 'KZ ZS10 Pro',
+  description: 'Neutral / IEM',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '329',
+  type: 'Audio',
+  name: 'Samson Q2U',
+  description: 'Dynamic',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '339',
+  type: 'Audio',
+  name: 'BEHRINGER U-Phoria UM2',
+  description: 'Audio Interface',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+//////// Monitors ////////
+{
+  id: '498',
+  type: 'Monitor',
+  name: 'Legion (Model #)',
+  description: '1080p / 240hz',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '499',
+  type: 'Monitor',
+  name: 'Dell (Model #)',
+  description: '1440p / 144hz',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+//////// EDC ////////
+{
+  id: '509',
+  type: 'EDC',
+  name: 'CJRB Pyrite',
+  description: 'Button Lock',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '519',
+  type: 'EDC',
+  name: 'Nitecore TINI 2',
+  description: '500 Lumens',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '529',
+  type: 'EDC',
+  name: 'Nomad Card Wallet',
+  description: 'Brown Horween Leather',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '539',
+  type: 'EDC',
+  name: 'Nomad Phone Case',
+  description: 'Brown Horween Leather',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '549',
+  type: 'EDC',
+  name: 'Refyne EP1',
+  description: 'Stainless Steel / Bolt Action',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+{
+  id: '559',
+  type: 'EDC',
+  name: 'Apple Airpods Pro 2',
+  description: 'V-Shaped / Wireless IEM',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
+    }
+  ],
+},
+//// Other ////
 {
   id: '600',
-  name: 'seven item',
-  description: 'description',
-  link: '#',
+  type: 'Other',
+  name: 'Herman Miller Aeron',
+  description: 'Size C',
   imgSrc: './assets/images/test.png',
   details: [
     {
       imgSrc: '',
-      caption: 'review text'
+      caption: ''
+    }
+  ],
+},
+{
+  id: '601',
+  type: 'Other',
+  name: 'Mazda 3',
+  description: '2022 / Carbon Edition',
+  imgSrc: './assets/images/test.png',
+  details: [
+    {
+      imgSrc: '',
+      caption: ''
     }
   ],
 },
