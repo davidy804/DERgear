4990c1b705bcba44c5a389abf2fc6c17568724aa
David Yuan <yua002@henrico.us>
2023-03-14 16:31:19 -0400

    update

--- src/app/shared/all-items.ts
+++ src/app/shared/all-items.ts
@@ -176,7 +176,7 @@ export const AllItems: MainModel[] = [
       <br> <br>
       <strong>Texture:</strong> <br> Smooth
       <br> <br>
-      <strong>Performance:</strong> <br> Medium static and medium dynamic friction. Works with all games and sensitivities.
+      <strong>Performance:</strong> <br> Medium static dynamic friction. Works with all games and sensitivities.
       <br> <br>
       <strong>Quality:</strong> <br> Lays flat, good stitching, good consistency, thick rubber base.
       <br> <br>
@@ -203,9 +203,9 @@ export const AllItems: MainModel[] = [
       <br> <br>
       <strong>Performance:</strong> <br> Low static and medium dynamic friction. Works with most games and sensitivities. Slower paced games like tac shooters are harder to make this mousepad work. Good mouse control is important with this mousepad. The micro movements are effortless because of the low static friction. This may cause instability and inconsistency if your mouse control is not good.
       <br> <br>
-      <strong>Quality:</strong> <br> Lays flat, decent stitching, decent consistency, bad rubber base. Surface slows down eventually, even with wash. The base does not stick very well. It slides around very easily, but not while gaming.
+      <strong>Quality:</strong> <br> Lays flat after a few weeks, decent stitching, decent consistency, bad rubber base. Surface slows down eventually, even with wash. The base does not stick very well. It slides around very easily, but not while gaming.
       <br> <br>
-      <strong>Conclusion:</strong> <br> The rubber base is the only problem with this mousepad. It's thin and slides around. My mousepad edge is starting to curl where my arm lays because my arm's heat is breaking down the rubber base. The actual performance of the mousepad is amazing.
+      <strong>Conclusions:</strong> <br> The rubber base is the only problem with this mousepad. It's thin and slides around. My mousepad edge is starting to curl where my arm lays because my arm's heat is breaking down the rubber base. The actual performance of the mousepad is amazing.
       <br> <br>
       <strong>Score:</strong> <br> 8/10
       `
@@ -221,7 +221,19 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Material:</strong> <br>
+      <br> <br>
+      <strong>Texture:</strong> <br> Rough? Not rough, but not smooth. Will not iritate skin, but not smooth like cloth.
+      <br> <br>
+      <strong>Performance:</strong> <br> Extremely low static and dynamic friction. For accurate tracking/flicking, this mousepad is not it. It just zooms.
+      <br> <br>
+      <strong>Quality:</strong> <br> For a ~$10 mousepad, the value and quality is insane. This is basically a Glide38 clone. The silicone base is good, but takes a long time to lay flat. It is not stitched, so it does fray.
+      <br> <br>
+      <strong>Conclusions:</strong> <br> It zooms. It's too fast for my usage. It feels like you are using a hardpad without the hardness. The quality is great for the price. It's worth a try if you can find it in stock.
+      <br> <br>
+      <strong>Score:</strong> <br> 6/10
+      `
     }
   ],
 },
@@ -234,7 +246,19 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Material:</strong> <br> Codura
+      <br> <br>
+      <strong>Texture:</strong> <br> Very rough
+      <br> <br>
+      <strong>Performance:</strong> <br> Low static and medium dynamic friction. Works well with most games. Low static friction requires good mouse control especially with higher sensitivity. The dynamic friction makes the mousepad perform more like a slower pad.
+      <br> <br>
+      <strong>Quality:</strong> <br> Lays flat, decent stitching, decent consistency, decent rubber base. Surface slows down after washes. Hard to wash because water and soap likes to stay in the pad, like a sponge. It comes in a few sizes, a deskpad size and a normal size. The normal size is smaller than other gaming pads. There is not enough depth for me, as I rest my arm on my desk/mousepad.
+      <br> <br>
+      <strong>Conclusions:</strong> <br> My preference combination of low static and medium dynamic friction. Mouse control is not an issue for me, and I prefer having the ability to micro-correct instantly. Higher dynamic friction makes it so when I 180 flick, it doesn't feel like I'm throwing my mouse across the room. This is definitely a great mousepad performance wise, but it just lacks in a few other areas. It's hard to wash, the size is small, and very rough.
+      <br> <br>
+      <strong>Score:</strong> <br> 8/10
+      `
     }
   ],
 },
@@ -247,7 +271,19 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Material:</strong> <br> Cloth
+      <br> <br>
+      <strong>Texture:</strong> <br> Smooth
+      <br> <br>
+      <strong>Performance:</strong> <br> High static and dynamic friction. Slow pad that gets even slower after water just looks at it. It feels great to use until humidity touches it. Humidity causes slow spots in this pad.
+      <br> <br>
+      <strong>Quality:</strong> <br> One of the lowest quality mousepads on the market. Humidity kills it. It gets dirty very quickly. It is not stitched, so the edges fray. The rubber base is decent.
+      <br> <br>
+      <strong>Conclusions:</strong> <br> This pad is very popular for no reason. It was widely available and pro gamers used this mousepad because of sponsorships, which popularized the mousepad. This mousepad is horrible if there is any humidity where you live. It instantly soaks up the water and slows down the pad. It makes the pad inconsistent and muddy. The actual performance is okay, if water never looked at it. There are much better options if you want a slow pad.
+      <br> <br>
+      <strong>Score:</strong> <br> 3/10
+      `
     }
   ],
 },
@@ -261,7 +297,17 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Keyboard:</strong> <br> IV Works AV4 Claymore
+      <br> <br>
+      <strong>Switches:</strong> <br> KTT Roses (Stock)
+      <br> <br>
+      <strong>Keycaps:</strong> <br> NicePBT Beach Day
+      <br> <br>
+      <strong>Sound Profile:</strong> <br> Loud, Clacky, Poppy
+      <br> <br>
+      <strong>Conclusions:</strong> <br> A high premium stacked acrylic alice style keyboard with a long pole sound profile. It has a great underglow and skeleton plate aesthetic.
+      `
     }
   ],
 },
@@ -274,7 +320,17 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Keyboard:</strong> <br> WindX65 (Tape mod)
+      <br> <br>
+      <strong>Switches:</strong> <br> Gateron Box Pinks (Lubed and Filmed)
+      <br> <br>
+      <strong>Keycaps:</strong> <br> NicePBT Classic Beige
+      <br> <br>
+      <strong>Sound Profile:</strong> <br> Quiet, Poppy, Marbly
+      <br> <br>
+      <strong>Conclusions:</strong> <br> A high premium 65% with a poppy sound profile. This board does not need to be tape modded. It is not hollow or metallic sounding at all. I tape modded this because this is a prototype keyboard with third party gaskets that do not fit well. The PCB shorts when pressed down hard enough. The tape mod prevents the shorting. It still sounds good with the tape mod. It gives it more of a marbly, PE foam sound.
+      `
     }
   ],
 },
@@ -287,7 +343,17 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Keyboard:</strong> <br> Wuque Studios Ikki Aurora Round 2
+      <br> <br>
+      <strong>Switches:</strong> <br> Wuque Studios Silent Linears (Stock)
+      <br> <br>
+      <strong>Keycaps:</strong> <br> Domikey Ghost
+      <br> <br>
+      <strong>Sound Profile:</strong> <br> Silent, Silicone, Raindrops
+      <br> <br>
+      <strong>Conclusions:</strong> <br> An entry level 65% with silent switches. The board itself is flexy and has a silicone sound signature. The silent switches emphasizes the silicone sound. The slight pop of the silent switches sounds like light raindrops hitting the windowsill. It's a very satisfying and relaxing sound. It feels like typing on clouds. It's smooth but has a slight heaviness to it that ends with a soft cushion.
+      `
     }
   ],
 },
@@ -300,7 +366,17 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Keyboard:</strong> <br> Glorious GMMK PRO
+      <br> <br>
+      <strong>Switches:</strong> <br> Broken-In Cherry MX Hyperglide Browns (Lubed and Filmed)
+      <br> <br>
+      <strong>Keycaps:</strong> <br> PBTFans White on Black
+      <br> <br>
+      <strong>Sound Profile:</strong> <br> Empty, Thock, Scratch
+      <br> <br>
+      <strong>Conclusions:</strong> <br> An entry level 75% with the signature Cherry scratch. This board lacks any character. No matter what switches you put in, it'll sound very similar. The typing feel is very firm as there is no flex or space in the keyboard. The Broken-In Browns are a very smooth light tactile with a scratchy sound. I would like to use these switches in another aluminum plate keyboard to get better feel of these switches.
+      `
     }
   ],
 },
@@ -380,7 +456,15 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Performance:</strong> <br> 240hz in comparison to 144hz is noticable. It feels like a 50% increase in smoothness. For reference, 144hz to 60hz feels like a 200% increase in smoothness. This is assuming you are getting enough frames to make use of the hertz. The colors on this IPS monitor are very vibrant. It gets very bright.
+      <br> <br>
+      <strong>Quality:</strong> <br> My monitor has a burn in issue. On certain applications or webpages, the monitor starts to LCD burn in. Lines start to burn into the display. A fix is to play a video for a few minutes to get rid of the burn in. This happens more often the brighter the monitor is. The monitor stand is sturdy and very adjustable.
+      <br> <br>
+      <strong>Conclusion:</strong> <br> Good specs, bad monitor. It has all the features of a great monitor; IPS, 240hz, and a good stand. The burn in issue is pretty annoying. It doesn't happen all the time, but when it does, it makes me want to buy a new monitor. The colors and brightness is too much for me. I prefer my Dell monitor's colors and brightness levels. The brightness of this monitor caused headaches for me. It would strain my eyes so much that I would get headaches every time I used it. I had to lower the brightness to ~20% and contrast to ~30%. I basically made this IPS look like a TN panel.
+      `
     }
   ],
 },
@@ -393,7 +477,15 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Specs:</strong> <br>
+      <br> <br>
+      <strong>Performance:</strong> <br> 1440p is so nice. You may think 1080p is enough, but that is because you haven't experienced 1440p. 1080p feels like you're using your Grandma's computer. 1440p gives so much room for productivity and media consumption. 144hz is good enough for most people.
+      <br> <br>
+      <strong>Quality:</strong> <br> My first monitor's LCD started to peel from the top after about 2 years. Dell sent a replacement and it has been working flawlessly for ~4+ years. The stand is solid with great adustability.
+      <br> <br>
+      <strong>Conclusion:</strong> <br> This is probably my favorite purchase in all my gear. I spent $500 for this monitor and I'd do it again. (You can get this monitor for a lot less nowadays) I went from 1080p 60hz to 1440p 144hz. The upgrade was huge. I have a 1080p 240hz monitor, and the lower resolution feels bad. 240hz compared to 144hz is not that big of a deal for me, despite playing mostly competitive games. My PC does not maintain 240 fps on all games, so I do not get 240hz all the time anyways. Dell has great colors and quality. This is a TN panel, but I like this monitor better than an IPS. I'd recommend this monitor or something with similar specs and quality to anyone.
+      `
     }
   ],
 },
@@ -407,7 +499,16 @@ export const AllItems: MainModel[] = [
   details: [
     {
       imgSrc: '',
-      caption: ''
+      caption: `
+      <strong>Material:</strong> <br> Stainless Steel
+      <br> <br>
+      <strong>Locking Action:</strong> <br> Button Lock
+      <br> <br>
+      <strong></strong> <br>
+      <br> <br>
+      <strong>Conclusions:</strong> <br>
+      <br> <br>
+      `
     }
   ],
 },
